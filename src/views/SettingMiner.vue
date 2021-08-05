593b79f7cf10a34bdb534af9dbdbf711e1dc5a73
ChenYi <yi.chen@intchains.com>
2021-08-05 11:22:04 +0800

    优化了组件布局，添加了控制ARGB风扇的函数

--- src/views/SettingMiner.vue
+++ src/views/SettingMiner.vue
@@ -54,9 +54,10 @@
             </v-row>
             <v-switch v-model="minerConfig.tempcontrol" :label="`${$t('MINER_TEMP')}（${$t('MINER_TEMP_NOTE')}）`" disabled></v-switch>
             <v-switch v-model="minerConfig.ledcontrol" :label="`${$t('MINER_LED')}（${$t('MINER_LED_NOTE')}）`" @change="minerDisable = false"></v-switch>
+
           </v-card-text>
           <v-card-actions>
-            <v-btn text color="primary" @click="updateMinerConfig" :loading="minerLoading" :disabled="minerDisable">{{ $t('MINER_APPLY') }}</v-btn>
+            <v-btn text color="primary" @click="updateMinerConfig()" :loading="minerLoading" :disabled="minerDisable">{{ $t('MINER_APPLY') }}</v-btn>
           </v-card-actions>
         </v-card>
 
@@ -139,33 +140,36 @@
     </v-dialog>
 
 
-      <!-- RGB风扇控制 -->
-      <v-container fluid>
-        <!-- 模式控制 -->
-        <v-card
-            flat color="transparent">
-          <v-subheader>模式选择</v-subheader>
-          <v-row  >
-            <v-col cols="8" >
-              <v-select
-                  v-model="select"
-                  :items="rgb_modules"
-                  item-text="state"
-                  item-value="abbr"
-                  label="RGB风扇模式"
-                  persistent-hint
-                  return-object
-                  single-line
-              ></v-select>
-            </v-col>
-          </v-row>
-        </v-card>
-        <!-- 亮度控制 -->
-        <v-card
-            flat
-            color="transparent"
-        >
-          <v-subheader>亮度调节</v-subheader>
+
+    <!-- RGB风扇设置 -->
+    <v-card outlined class="mt-4">
+      <v-card-title @click="openManual"> {{ $t('ARGB_FAN_SETTINGS') }} </v-card-title>
+      <v-card-text>
+
+        <!-- RGB风扇控制 -->
+        <v-container >
+          <!-- 模式控制 -->
+          <v-subheader>{{ $t('MODE_SELECT') }}</v-subheader>
+          <v-card-text
+          >
+            <v-row   >
+              <v-col
+                  class="d-flex"
+                  cols="8"
+                  sm="6"
+              >
+                <v-select
+                    :items="rgb_modules"
+                    @change="setMode('0x01')"
+                ></v-select>
+              </v-col>
+
+            </v-row>
+          </v-card-text>
+
+          <!-- 亮度控制 -->
+
+          <v-subheader>{{ $t('BRIGHTNESS_SELECT') }}</v-subheader>
           <v-card-text>
             <v-row>
               <v-col class="pr-4">
@@ -174,7 +178,9 @@
                     class="align-center"
                     :max="max"
                     :min="min"
+                    value=""
                     hide-details
+                    @change="brightnessValue=setBrightness('0xff')"
                 >
                   <template v-slot:append>
                     <v-text-field
@@ -190,29 +196,31 @@
               </v-col>
             </v-row>
           </v-card-text>
-        </v-card>
-        <!-- 速度控制 -->
-        <v-card
-            flat
-            color="transparent"
-        >
-          <v-subheader>速度调节</v-subheader>
+
+          <!-- 速度控制 -->
+
+          <v-subheader>{{ $t('SPEED_SELECT') }}</v-subheader>
           <v-card-text>
             <v-slider
                 v-model="speed"
-                :tick-labels="speeds"
+                :tick-labels="speedLables"
                 :max="2"
                 step="1"
                 ticks="always"
                 tick-size="3"
+                @change="speedValue = setSpeed('0x10')"
             ></v-slider>
           </v-card-text>
-        </v-card>
-
-      </v-container>
 
 
+          <v-btn text color="primary" @click="sendData(modeValue,brightnessValue,speedValue)" >{{ $t('MINER_APPLY') }}</v-btn>
 
+        </v-container>
+
+      </v-card-text>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
 
 
 
@@ -221,11 +229,33 @@
 
 </template>
 
-<script>
+<script >
 import { mapGetters } from 'vuex'
 import { getmcbsetting, getmcbpools, getmcbalgosetting, setmcbsetting, setmcbalgosetting, deletepool, setmcbpools, setmcbnewpool, gettutorial } from '../api/mcb'
 import draggable from 'vuedraggable'
 import Lock from '../components/Lock.vue'
+
+/* 十六进制字符串 -> 十进制数字 */
+function parseHex(str) {
+  return parseInt(str,16);
+}
+
+
+/* 十进制数字 -> 十六进制数字 */
+function decToHex(params) {
+  typeof params == "string" ? params = Number(params) : ''
+  return '0x' + params.toString(16);
+}
+
+/* 获取校验码 */
+function getCheckCode(start,type,mode,brightness,speed,) {
+  let result = (start ^ type);
+  result ^= mode;
+  result ^= brightness;
+  result ^= speed;
+  return  '0x' + result.toString(16);
+}
+
 export default {
   components: {
     draggable,
@@ -266,36 +296,45 @@ export default {
         viabtc: require('../assets/images/pools/viabtc.svg'),
         poolin: require('../assets/images/pools/poolin.svg')
       },
-      /* rgb模式 */
-      select: { state: '彩色循环模式', abbr: 'module1' },
-      rgb_modules: [
-        { state: '彩色循环模式', abbr: 'module1' },
-        { state: '追光模式', abbr: 'module2' },
-        { state: '能量模式', abbr: 'module3' },
-        { state: '堆积模式', abbr: 'module4' },
-        { state: '呼吸模式', abbr: 'module5' },
-        { state: '渐变模式', abbr: 'module6' },
-        { state: '太极旋转模式', abbr: 'module7' },
-        { state: '七彩飘带模式', abbr: 'module8 不可自定义' },
-      ],
+
       /* 亮度调节 */
       min: 0,
       max: 255,
       slider: 40,
       /* 速度调节 */
       speed:0,
-      speeds: [
-        '慢速',
-        '中速',
-        '快速'
-      ],
+
+      modeValue:0x01,
+      brightnessValue:0xff,
+      speedValue:0x10,
     }
   },
   computed: {
     ...mapGetters({
       isLogin: 'isLogin',
       minerInfo: 'minerInfo'
-    })
+    }),
+    /* 模式调节 */
+    rgb_modules() {
+      return [
+        this.$t('COLORFUL_LOOP_MODE'),
+        this.$t('LIGHT_TRACKING_MODE'),
+        this.$t('ENERGY_MODE'),
+        this.$t('ACCUMULATION_MODE'),
+        this.$t('BREATHING_MODE'),
+        this.$t('GRADIENT_MODE'),
+        this.$t('TAI_CHI_ROTATION_MODE'),
+        this.$t('COLORFUL_STREAMERS'),
+      ]
+    },
+    speedLables() {
+      return [
+        this.$t('SLOW_SPEED'),
+        this.$t('MEDIUM_SPEED'),
+        this.$t('FAST_SPEED'),
+      ]
+    },
+
   },
   created() {
     this.pageInit()
@@ -324,28 +363,28 @@ export default {
 
     getMinerConfig() {
       getmcbsetting()
-        .then((res) => {
-          this.minerConfig = res
-          for (let index = 0; index < res.powerplans.length; index++) {
-            this.powerPlans.push({
-              text: this.minerLevel[res.powerplans[index].level],
-              value: res.powerplans[index].level
-            })
-          }
-        })
-        .catch(() => {
-          this.$snackbar.error(this.$t('WARNING_GETMCBSETTINGS'))
-        })
+          .then((res) => {
+            this.minerConfig = res
+            for (let index = 0; index < res.powerplans.length; index++) {
+              this.powerPlans.push({
+                text: this.minerLevel[res.powerplans[index].level],
+                value: res.powerplans[index].level
+              })
+            }
+          })
+          .catch(() => {
+            this.$snackbar.error(this.$t('WARNING_GETMCBSETTINGS'))
+          })
     },
 
     getPoolConfig() {
       getmcbpools()
-        .then((res) => {
-          this.pools = res
-        })
-        .catch(() => {
-          this.$snackbar.error(this.$t('WARNING_GETMCBPOOLS'))
-        })
+          .then((res) => {
+            this.pools = res
+          })
+          .catch(() => {
+            this.$snackbar.error(this.$t('WARNING_GETMCBPOOLS'))
+          })
     },
 
     getMinerAlgo() {
@@ -367,13 +406,13 @@ export default {
     updateMinerConfig() {
       this.minerLoading = true
       setmcbsetting(this.minerConfig)
-        .then(() => {
-          this.minerLoading = false
-        })
-        .catch(() => {
-          this.minerLoading = false
-          this.$snackbar.error(this.$t('WARNING_SETMCBSETTINGS'))
-        })
+          .then(() => {
+            this.minerLoading = false
+          })
+          .catch(() => {
+            this.minerLoading = false
+            this.$snackbar.error(this.$t('WARNING_SETMCBSETTINGS'))
+          })
     },
 
     updateMinerAlgo() {
@@ -382,27 +421,27 @@ export default {
         algo_select: this.algos.algo_select
       }
       setmcbalgosetting(data)
-        .then(() => {
-          this.algoLoading = false
-        })
-        .catch(() => {
-          this.algoLoading = false
-          this.$snackbar.error(this.$t('CHANGE_ALGO_FAIL'))
-        })
+          .then(() => {
+            this.algoLoading = false
+          })
+          .catch(() => {
+            this.algoLoading = false
+            this.$snackbar.error(this.$t('CHANGE_ALGO_FAIL'))
+          })
     },
 
     deletePoolConfig() {
       this.poolLoading = true
       deletepool(this.poolConfig)
-        .then(() => {
-          this.poolLoading = false
-          this.getPoolConfig()
-          this.delPoolDialog = false
-        })
-        .catch(() => {
-          this.poolLoading = false
-          this.$snackbar.error(this.$t('WARNING_DELMCBPOOLS'))
-        })
+          .then(() => {
+            this.poolLoading = false
+            this.getPoolConfig()
+            this.delPoolDialog = false
+          })
+          .catch(() => {
+            this.poolLoading = false
+            this.$snackbar.error(this.$t('WARNING_DELMCBPOOLS'))
+          })
     },
 
     submitPoolConfig() {
@@ -413,21 +452,21 @@ export default {
           pass: this.poolConfig.pass
         }
         setmcbnewpool(data)
-          .then(() => {
-            this.dialog = false
-            this.pools.push({
-              url: 'stratum+tcp://' + this.poolConfig.url,
-              user: this.poolConfig.user,
-              pass: this.poolConfig.pass,
-              dragid: this.pools.length
+            .then(() => {
+              this.dialog = false
+              this.pools.push({
+                url: 'stratum+tcp://' + this.poolConfig.url,
+                user: this.poolConfig.user,
+                pass: this.poolConfig.pass,
+                dragid: this.pools.length
+              })
+              setTimeout(() => {
+                this.getPoolConfig()
+              }, 5000)
             })
-            setTimeout(() => {
-              this.getPoolConfig()
-            }, 5000)
-          })
-          .catch(() => {
-            this.$snackbar.error(this.$t('WARNING_SETNEWPOOL'))
-          })
+            .catch(() => {
+              this.$snackbar.error(this.$t('WARNING_SETNEWPOOL'))
+            })
       }
     },
 
@@ -436,12 +475,12 @@ export default {
         this.pools[index]['pool-priority'] = index
       }
       setmcbpools(this.pools)
-        .then(() => {
-          this.getPoolConfig()
-        })
-        .catch(() => {
-          this.$snackbar.error(this.$t('WARNING_SETMCBPOOLS'))
-        })
+          .then(() => {
+            this.getPoolConfig()
+          })
+          .catch(() => {
+            this.$snackbar.error(this.$t('WARNING_SETMCBPOOLS'))
+          })
     },
 
     getMinerTutorial() {
@@ -486,14 +525,68 @@ export default {
     },
 
 
+    /* 设置亮度状态码（Hex）*/
+    setBrightness(brightness) {
+      return decToHex(parseHex(brightness));
+      // eslint-disable-next-line no-unreachable
+    },
 
+    /* 设置速度状态码(Hex) */
+    setSpeed(speed) {
+      return decToHex(parseHex(speed));
+    },
+
+    /* 设置模式状态码 */
+    setMode(mode) {
+      return decToHex(parseHex(mode));
+    },
+
+    /* 发送数据 */
+    sendData(mode,brightness,speed) {
+      const SerialPort = require("serialport");
+      let checkCode = getCheckCode(0xaa,0x01,mode,brightness,speed);
+      const data = [0xaa, 0x01, mode, brightness, speed, checkCode];
+      let serialPort = new SerialPort( //设置串口属性
+
+          "COM3", {
+
+            baudRate: 9600, //波特率
+
+            dataBits: 8, //数据位
+
+            parity: 'none', //奇偶校验
+
+            stopBits: 2, //停止位
+
+            flowControl: false ,
+
+            autoOpen:false // 自动打开
+
+          }, false);
+
+      serialPort.open();
+
+      serialPort.on('open',() => {
+        console.log("串口已打开，准备写入数据")
+        serialPort.write(data);
+      })
+
+      serialPort.on('readable', function () {
+        console.log('Data:', serialPort.read())
+      })
+
+
+      serialPort.on('data',(data) => {
+
+        console.log("命令已经发出")
+        console.log("回应的数据: " + data)
+        serialPort.close();
+      })
+    }
   }
+
+
 }
-
-
-
-
-
 </script>
 
 <style lang="scss">
